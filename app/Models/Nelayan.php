79b925d3ddbc5c0a99205a77f26c8c7031773bd8
Fajarrosyidi24 <fishapp73@gmail.com>
2024-11-03 00:02:58 +0700

    saya telah menyelesaikan fitur profile&pengaturan di akun nelayan

--- app/Models/Nelayan.php
+++ app/Models/Nelayan.php
@@ -2,27 +2,28 @@
 
 namespace App\Models;
 
+use GuzzleHttp\Psr7\Request;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\PasswordRequest;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use App\Http\Requests\NelayanRegistrationRequest;
-use App\Http\Requests\PasswordRequest;
-use GuzzleHttp\Psr7\Request;
+use Mockery\Generator\StringManipulation\Pass\Pass;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Mockery\Generator\StringManipulation\Pass\Pass;
 
 class Nelayan extends Authenticatable
 {
     use HasFactory, HasApiTokens, Notifiable;
 
-     /**
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $guard ='nelayan';
-     protected $fillable = [
+    protected $guard = 'nelayan';
+    protected $fillable = [
         'name',
         'email',
         'password',
@@ -45,11 +46,11 @@ class Nelayan extends Authenticatable
      * The attributes that should be cast.
      *
      * @var array<string, string>
-     */  
+     */
 
 
-     public static function createNelayan(NelayanRegistrationRequest $request)
-     {
+    public static function createNelayan(NelayanRegistrationRequest $request)
+    {
         $validatedData = $request->validated();
 
         return self::create([
@@ -57,11 +58,10 @@ class Nelayan extends Authenticatable
             'email' => $request->validated()['email'],
             'status' => 'pending',
         ]);
+    }
 
-     }
-
-     public static function createpassword(PasswordRequest $request, $email)
-     {
+    public static function createpassword(PasswordRequest $request, $email)
+    {
         $validatedData = $request->validated();
         $nelayan = Nelayan::where('email', $email)->first();
         if ($nelayan) {
@@ -70,29 +70,56 @@ class Nelayan extends Authenticatable
                 'remember_token' => null,
                 'status' => 'terdaftar',
             ]);
-    
+
             return $nelayan; // Kembalikan nelayan yang telah diperbarui
         }
-    
-        return null;
-     }
 
-     public function detailProfile()
+        return null;
+    }
+
+    public function detailProfile()
     {
         return $this->hasOne(NelayanProfile::class);
     }
 
     public static function emailnelayan($request)
-{
-    $request->validate([
-        'email' => 'required|email',
-    ], [
-        'email.required' => 'Email wajib diisi',
-        'email.email' => 'Format email tidak valid',
-    ]);
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ], [
+            'email.required' => 'Email wajib diisi',
+            'email.email' => 'Format email tidak valid',
+        ]);
 
-    $email = $request->input('email');
-    $exists = self::where('email', $email)->exists();
-    return $exists ? $email : false;
-}
+        $email = $request->input('email');
+        $exists = self::where('email', $email)->exists();
+        return $exists ? $email : false;
+    }
+
+    public static function updateprofile(array $data, $userId)
+    {
+        $nelayan = Nelayan::find($userId);
+        if ($nelayan) {
+            $nelayan->name = $data['name'];
+            $nelayan->detailprofile->tempat_lahir = $data['tempat_lahir'];
+            $nelayan->detailprofile->tanggal_lahir = $data['tanggal_lahir'];
+            $nelayan->detailprofile->jenis_kelamin = $data['jenis_kelamin'];
+            $nelayan->detailprofile->no_telepon = $data['nomer_telepon'];
+            $nelayan->detailprofile->kabupaten = $data['district'];
+            $nelayan->detailprofile->kecamatan = $data['sub_district'];
+            $nelayan->detailprofile->desa = $data['desa'];
+            $nelayan->detailprofile->dusun = $data['dusun'];
+            $nelayan->detailprofile->rt = $data['rt'];
+            $nelayan->detailprofile->rw = $data['rw'];
+            $nelayan->detailprofile->code_pos = $data['code_pos'];
+            $nelayan->detailprofile->nama_kapal = $data['nama_kapal'];
+            $nelayan->detailprofile->jenis_kapal = $data['jenis_kapal'];
+            $nelayan->detailprofile->jumlah_abk = $data['jumlah_abk'];
+    
+            // Simpan perubahan
+            $nelayan->save();
+            $nelayan->detailprofile->save();
+        }
+        return $nelayan;
+    }    
 }
