0859ab22aefd9616119121152159cef59d3ccc6a
anzaayyy <rizkidwi1140@gmail.com>
2024-11-13 01:05:33 +0700

    test rama finish

--- app/Http/Requests/SeafoodRequest.php
+++ app/Http/Requests/SeafoodRequest.php
@@ -26,7 +26,7 @@ class SeafoodRequest extends FormRequest
             'type' => 'required|string', // Jenis seafood harus diisi dan berupa string
             'quantity' => 'required|numeric|min:1', // Jumlah harus diisi, berupa angka, dan minimal 1 kg
             'price' => 'required|numeric|min:1000', // Harga harus diisi, berupa angka, dan tidak boleh negatif
-            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:10240',
         ];
     }
 
@@ -41,21 +41,21 @@ class SeafoodRequest extends FormRequest
         'name.required' => 'Nama seafood harus diisi.',
         'name.string' => 'Nama seafood harus berupa teks.',
         'name.max' => 'Nama seafood maksimal 255 karakter.',
-        
+
         'type.required' => 'Jenis seafood harus dipilih.',
         'type.string' => 'Jenis seafood harus berupa teks.',
-        
+
         'quantity.required' => 'Jumlah seafood harus diisi.',
         'quantity.numeric' => 'Jumlah seafood harus berupa angka.',
         'quantity.min' => 'Jumlah seafood minimal 1 kg.',
-        
+
         'price.required' => 'Harga seafood harus diisi.',
         'price.numeric' => 'Harga seafood harus berupa angka.',
         'price.min' => 'Harga seafood tidak boleh negatif. dan minimal Rp. 1000',
-        
+
         'photo.image' => 'File yang diunggah harus berupa gambar.',
         'photo.mimes' => 'Gambar harus berformat: jpeg, png, jpg, gif.',
-        'photo.max' => 'Ukuran gambar maksimal 2MB.',
+        'photo.max' => 'Ukuran gambar maksimal 10MB.',
         ];
     }
 }
